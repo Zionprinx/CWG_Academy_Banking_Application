ea9af94f129b91b234fdc047ca15792598e68d92
Zionprinx <90899598+Zionprinx@users.noreply.github.com>
2022-03-12 23:00:07 +0100

    Add files via upload

---
+++ TestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cwg_academy.testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
